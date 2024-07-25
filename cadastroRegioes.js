571e5f23baf3320b9f630eba7e47942ef59c4504
gbcapri <gbcapri@gmail.com>
2024-07-25 11:13:29 -0300

    Adicionado alguma coisa

--- cadastroRegioes.js
+++ cadastroRegioes.js
@@ -1,10 +1,11 @@
 const prompt = require("prompt-sync")();
 const nomeInvalido = (nome) => nome == "";
-const indiceInvalido = indice => indice < 0 || indice >= pais.length || isNaN(indice)
+const indiceInvalido = (indice) =>
+  indice < 0 || indice >= pais.length || isNaN(indice);
 const pais = [];
 //-----------------------------------------------------------------------
 const regiao = () => {
-  let rega = {}; // não posso adicionar atributos em algo indefinido 
+  let rega = {}; // não posso adicionar atributos em algo indefinido
 
   while (true) {
     rega.cidade = prompt("Qual é o nome da cidade? ");
@@ -60,60 +61,69 @@ const listagem = () => {
 };
 //-----------------------------------------------------------------------
 const procurar = () => {
-    let achou = prompt("Digite a cidade da região que deseja encontrar: ");
-    let cidadeEncontrada = false;
-    pais.forEach((rega, indice) => {
-      if (rega.cidade === achou) {
-        cidadeEncontrada = true;
-        console.log("Informações sobre a região:");
-        console.log("Número da região: " + (indice + 1));
-        console.log("Nome da cidade: " + rega.cidade);
-        console.log("Nome do estado: " + rega.estado);
-        console.log("Nome da região: " + rega.regiao);
-        console.log("Nome do país: " + rega.pais);
-        console.log("");
-      }
-    });
-    if (!cidadeEncontrada) {
-      console.log("Cidade não encontrada.");
+  let achou = prompt("Digite a cidade da região que deseja encontrar: ");
+  let cidadeEncontrada = false;
+  pais.forEach((rega, indice) => {
+    if (rega.cidade === achou) {
+      cidadeEncontrada = true;
+      console.log("Informações sobre a região:");
+      console.log("Número da região: " + (indice + 1));
+      console.log("Nome da cidade: " + rega.cidade);
+      console.log("Nome do estado: " + rega.estado);
+      console.log("Nome da região: " + rega.regiao);
+      console.log("Nome do país: " + rega.pais);
+      console.log("");
     }
-  };
+  });
+  if (!cidadeEncontrada) {
+    console.log("Cidade não encontrada.");
+  }
+};
 //-----------------------------------------------------------------------
 const atualizar = () => {
-    while (true) {
-      if (pais.length == 0) {
-        console.log("Lista de regiões vazia");
-        break;
-      }
-      listagem();
-      const indice = parseInt(prompt("Qual é o índice da região que deseja atualizar? ")) - 1;
-      if (indiceInvalido(indice)) {
-        console.log("Índice inválido");
-      } else {
-        const rega = pais[indice];
-        console.log("Somente aperte ENTER se não quiser atualizar");
-        const novaCidade = prompt("Qual é o novo nome da cidade? (atual: "+rega.cidade+") ");
-        if (!nomeInvalido(novaCidade)) {
-          rega.cidade = novaCidade;
-        }
-        const novoEstado = prompt("Qual é o novo nome do estado? (atual: "+rega.estado+") ");
-        if (!nomeInvalido(novoEstado)) {
-          rega.estado = novoEstado;
-        }
-        const novaRegiao = prompt("Qual é a nova região do estado? (atual: "+rega.regiao+") ");
-        if (!nomeInvalido(novaRegiao)) {
-          rega.regiao = novaRegiao;
-        }
-        const novoPais = prompt("Qual é o novo nome do país? (atual: "+rega.pais+") ");
-        if (!nomeInvalido(novoPais)) {
-          rega.pais = novoPais;
-        }
-        pais[indice] = rega;
-        console.log("Região atualizada com sucesso.");
-        break;
-      }
+  while (true) {
+    if (pais.length == 0) {
+      console.log("Lista de regiões vazia");
+      break;
     }
-  };
+    listagem();
+    const indice =
+      parseInt(prompt("Qual é o índice da região que deseja atualizar? ")) - 1;
+    if (indiceInvalido(indice)) {
+      console.log("Índice inválido");
+    } else {
+      const rega = pais[indice];
+      console.log("Somente aperte ENTER se não quiser atualizar");
+      const novaCidade = prompt(
+        "Qual é o novo nome da cidade? (atual: " + rega.cidade + ") "
+      );
+      if (!nomeInvalido(novaCidade)) {
+        rega.cidade = novaCidade;
+      }
+      const novoEstado = prompt(
+        "Qual é o novo nome do estado? (atual: " + rega.estado + ") "
+      );
+      if (!nomeInvalido(novoEstado)) {
+        rega.estado = novoEstado;
+      }
+      const novaRegiao = prompt(
+        "Qual é a nova região do estado? (atual: " + rega.regiao + ") "
+      );
+      if (!nomeInvalido(novaRegiao)) {
+        rega.regiao = novaRegiao;
+      }
+      const novoPais = prompt(
+        "Qual é o novo nome do país? (atual: " + rega.pais + ") "
+      );
+      if (!nomeInvalido(novoPais)) {
+        rega.pais = novoPais;
+      }
+      pais[indice] = rega;
+      console.log("Região atualizada com sucesso.");
+      break;
+    }
+  }
+};
 //-----------------------------------------------------------------------
 const remover = () => {
   let achou = prompt("Digite a cidade da região que deseja encontrar: ");
